edc41741a5ae2ebc19303b4cb7e94d4e7332c9f5
Morior <jordi.grados@escapadarural.com>
2023-09-22 14:40:08 +0200

    Adding multiple money to the list of money

--- atm_machine/tests/Services/MoneyFormatterTest.php
+++ atm_machine/tests/Services/MoneyFormatterTest.php
@@ -50,4 +50,34 @@ class MoneyFormatterTest extends TestCase
         $expectedResult = '2 bills of 200.' . PHP_EOL;
         $this->assertEquals($expectedResult, $result);
     }
+
+    public function testGivenAListOfMoneyWithBillsAndCoinsShouldFormatIntoString()
+    {
+        $quantity = 2;
+        $type = 'bill';
+        $amount = 200;
+        $money1 = new Money($quantity, $type, $amount);
+        $quantity = 1;
+        $type = 'bill';
+        $amount = 20;
+        $money2 = new Money($quantity, $type, $amount);
+        $quantity = 1;
+        $type = 'bill';
+        $amount = 10;
+        $money3 = new Money($quantity, $type, $amount);
+        $quantity = 2;
+        $type = 'coin';
+        $amount = 2;
+        $money4 = new Money($quantity, $type, $amount);
+        $listOfMoney = [
+            $money1, $money2, $money3, $money4,
+        ];
+        $result = $this->sut->format($listOfMoney);
+
+        $expectedResult = '2 bills of 200.' . PHP_EOL
+            . '1 bill of 20.' . PHP_EOL
+            . '1 bill of 10.' . PHP_EOL
+            . '2 coins of 2.' . PHP_EOL;
+        $this->assertEquals($expectedResult, $result);
+    }
 }
