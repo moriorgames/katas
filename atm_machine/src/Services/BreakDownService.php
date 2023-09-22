da9767cb81848ccafae97c6492e6d6e6dbdadc50
Morior <jordi.grados@escapadarural.com>
2023-09-22 15:04:59 +0200

    Working on  the breakdown service

--- atm_machine/src/Services/BreakDownService.php
+++ atm_machine/src/Services/BreakDownService.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Domain\AllowedAmount;
 use App\Domain\Money;
+use App\Domain\MoneyType;
 
 class BreakDownService
 {
+    private const BREAK_ORDER = [
+        [AllowedAmount::AMOUNT_1]
+    ];
+
     /**
      * @param int $quantity
      * @return Money[]
      */
     public function break(int $quantity): array
     {
-        return [];
+        return [new Money($quantity, MoneyType::Coin, AllowedAmount::AMOUNT_1)];
     }
 }
--- atm_machine/tests/Services/BreakDownServiceTest.php
+++ atm_machine/tests/Services/BreakDownServiceTest.php
@@ -4,13 +4,27 @@ declare(strict_types=1);
 
 namespace Tests\Services;
 
+use App\Domain\AllowedAmount;
+use App\Domain\Money;
+use App\Domain\MoneyType;
+use App\Services\BreakDownService;
 use PHPUnit\Framework\TestCase;
 
 class BreakDownServiceTest extends TestCase
 {
+    private BreakDownService $sut;
 
-    public function test_()
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        $this->sut = new BreakDownService();
+    }
+
+    public function testGivenOneWillReturnOneCoinOfOne()
+    {
+        $quantity = 1;
+        $result = $this->sut->break($quantity);
+
+        $this->assertEquals(new Money(1, MoneyType::Coin, AllowedAmount::AMOUNT_1), $result[0]);
     }
 }
